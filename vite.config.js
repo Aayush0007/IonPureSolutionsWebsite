42207b50413046d934f2d2c554abde8371cc0931
Aayush0007 <sharmaaayush2310@gmail.com>
2026-02-13 22:30:28 +0530

    Improvise version 23

--- vite.config.js
+++ vite.config.js
@@ -7,24 +7,28 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['SplashScreenLogo.png', 'Logo.png'],
+      includeAssets: ['SplashScreenLogo.png'], 
       manifest: {
-        name: 'Ion Pure Solutions',
-        short_name: 'IonPure',
-        description: 'Premium Alkaline & Hydrogen Water Technology',
-        theme_color: '#F1F8E1', // Matches ionMint palette
+        // This is the name displayed under the icon on the home screen
+        name: 'ION PURE SOLUTIONS', 
+        short_name: 'IPS',
+        description: 'Advanced Alkaline & Hydrogen Water Tech',
+        theme_color: '#F1F8E1',
         background_color: '#FDFDFD',
         display: 'standalone',
+        // Mapping the icon to your public folder asset
         icons: [
           {
-            src: '/SplashScreenLogo.png', // Reference: Master Documentation Section 3
+            src: '/SplashScreenLogo.png',
             sizes: '192x192',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any maskable'
           },
           {
             src: '/SplashScreenLogo.png',
             sizes: '512x512',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any maskable'
           }
         ]
       }
